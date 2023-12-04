3fea124fedfacb63fb300a7245d8f74c0f99af10
Alain Kaiser <kaiseralain1@gmail.com>
2023-12-04 23:19:21 +0100

    wip: day 4, part 1

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -1,6 +1,8 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode;
 
-public sealed class Day04 : BaseDay
+public sealed partial class Day04 : BaseDay
 {
     private readonly string[] _input;
 
@@ -11,11 +13,61 @@ public sealed class Day04 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        throw new NotImplementedException();
+        var sum = 0;
+
+        foreach (var game in _input)
+        {
+            var gameSum = 0;
+
+            var splitOne = game.Split(":");
+            var numbersSplit = splitOne[1].Split("|");
+            var winningNumbers = Regex.Split(numbersSplit[0].Trim(), @"\D+");
+            var numbersIHave = Regex.Split(numbersSplit[1].Trim(), @"\D+");
+
+            var numbersFound = 0;
+
+            foreach (var numberIHave in numbersIHave)
+            {
+                if (winningNumbers.Contains(numberIHave))
+                {
+                    numbersFound++;
+                    gameSum = numbersFound == 1 ? 1 : (gameSum * 2);
+                }
+            }
+
+            sum += gameSum;
+        }
+
+        return new ValueTask<string>(sum.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        var sum = 0;
+
+        foreach (var game in _input)
+        {
+            var gameSum = 0;
+
+            var splitOne = game.Split(":");
+            var numbersSplit = splitOne[1].Split("|");
+            var winningNumbers = Regex.Split(numbersSplit[0].Trim(), @"\D+");
+            var numbersIHave = Regex.Split(numbersSplit[1].Trim(), @"\D+");
+
+            var numbersFound = 0;
+
+            foreach (var numberIHave in numbersIHave)
+            {
+                if (winningNumbers.Contains(numberIHave))
+                {
+                    numbersFound++;
+                    gameSum = numbersFound == 1 ? 1 : (gameSum * 2);
+                }
+            }
+
+            Console.WriteLine(numbersFound);
+        }
+
+        return new ValueTask<string>(sum.ToString());
     }
 }
