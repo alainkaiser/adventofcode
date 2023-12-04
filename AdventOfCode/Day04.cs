ee794db97c1de54e8b2c94d00be50d2f8f6ec7e3
Alain Kaiser <kaiseralain1@gmail.com>
2023-12-05 00:11:39 +0100

    wip: part 2 day 4, refactor part 1

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -11,63 +11,94 @@ public sealed partial class Day04 : BaseDay
         _input = File.ReadAllLines(InputFilePath);
     }
 
+    private record Card(int CardId, int Matches, int Points = 0);
+
     public override ValueTask<string> Solve_1()
     {
-        var sum = 0;
-
-        foreach (var game in _input)
-        {
-            var gameSum = 0;
-
-            var splitOne = game.Split(":");
-            var numbersSplit = splitOne[1].Split("|");
-            var winningNumbers = Regex.Split(numbersSplit[0].Trim(), @"\D+");
-            var numbersIHave = Regex.Split(numbersSplit[1].Trim(), @"\D+");
-
-            var numbersFound = 0;
-
-            foreach (var numberIHave in numbersIHave)
-            {
-                if (winningNumbers.Contains(numberIHave))
-                {
-                    numbersFound++;
-                    gameSum = numbersFound == 1 ? 1 : (gameSum * 2);
-                }
-            }
-
-            sum += gameSum;
-        }
-
-        return new ValueTask<string>(sum.ToString());
+        var sumOfPoints = GetCards().Sum(x => x.Points);
+        
+        return new ValueTask<string>(sumOfPoints.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var sum = 0;
-
-        foreach (var game in _input)
+        var wonCards = new Dictionary<int, int>();
+        
+        var cards = GetCards();
+        foreach (var card in cards)
         {
-            var gameSum = 0;
+            if (wonCards.ContainsKey(card.CardId))
+            {
+                wonCards[card.CardId]++;
+            }
+            else
+            {
+                wonCards.Add(card.CardId, 1);
+            }
+            
+            // If the card has no matches, skip it
+            if(card.Matches == 0) continue;
+            ProcessMatchingCard(card);
+        }
 
+        var sum = wonCards.Sum(x => x.Value);
+        
+        return new ValueTask<string>(sum.ToString());
+
+        void ProcessMatchingCard(Card card)
+        {
+            // Get the won copies from the original cards list
+            var copies = cards
+                .Skip(card.CardId)
+                .Take(card.Matches);
+            
+            // Iterate through the won copies and add them to the wonCards dictionary
+            foreach (var copy in copies)
+            {
+                if (wonCards.ContainsKey(copy.CardId))
+                {
+                    wonCards[copy.CardId]++;
+                }
+                else
+                {
+                    wonCards.Add(copy.CardId, 1);
+                }
+
+                // Recursive call for copies with matches
+                if (copy.Matches > 0)
+                {
+                    ProcessMatchingCard(copy);
+                }
+            }
+        }
+    }
+
+    private List<Card> GetCards()
+    {
+        var cards = new List<Card>();
+        
+        for (var i = 0; i < _input.Length; i++)
+        {
+            var game = _input[i];
             var splitOne = game.Split(":");
             var numbersSplit = splitOne[1].Split("|");
             var winningNumbers = Regex.Split(numbersSplit[0].Trim(), @"\D+");
             var numbersIHave = Regex.Split(numbersSplit[1].Trim(), @"\D+");
 
-            var numbersFound = 0;
+            var points = numbersIHave
+                .Where(numberIHave => winningNumbers.Contains(numberIHave))
+                .Select((number, index) => new { Number = number, Index = index })
+                .Aggregate(0, (total, next) => next.Index == 0 ? 1 : total * 2);
 
-            foreach (var numberIHave in numbersIHave)
-            {
-                if (winningNumbers.Contains(numberIHave))
-                {
-                    numbersFound++;
-                    gameSum = numbersFound == 1 ? 1 : (gameSum * 2);
-                }
-            }
+            
+            var matches = (from numberIHave in numbersIHave
+                where winningNumbers.Contains(numberIHave)
+                select numberIHave)
+                .Count();
 
-            Console.WriteLine(numbersFound);
+            cards.Add(new Card(i + 1, matches, points));
         }
-
-        return new ValueTask<string>(sum.ToString());
+        
+        return cards;
     }
 }
