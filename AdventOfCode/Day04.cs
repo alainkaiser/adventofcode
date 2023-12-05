f2b46e7a82ce93360e7cd4e53d40aba76f1842b2
Alain Kaiser <kaiseralain1@gmail.com>
2023-12-05 08:42:25 +0100

    wip: simplified day4 a bit

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -16,33 +16,24 @@ public sealed partial class Day04 : BaseDay
     public override ValueTask<string> Solve_1()
     {
         var sumOfPoints = GetCards().Sum(x => x.Points);
-        
+
         return new ValueTask<string>(sumOfPoints.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        var wonCards = new Dictionary<int, int>();
-        
+        var sum = 0;
+
         var cards = GetCards();
         foreach (var card in cards)
         {
-            if (wonCards.ContainsKey(card.CardId))
-            {
-                wonCards[card.CardId]++;
-            }
-            else
-            {
-                wonCards.Add(card.CardId, 1);
-            }
-            
+            sum++;
+
             // If the card has no matches, skip it
-            if(card.Matches == 0) continue;
+            if (card.Matches == 0) continue;
             ProcessMatchingCard(card);
         }
 
-        var sum = wonCards.Sum(x => x.Value);
-        
         return new ValueTask<string>(sum.ToString());
 
         void ProcessMatchingCard(Card card)
@@ -51,18 +42,11 @@ public sealed partial class Day04 : BaseDay
             var copies = cards
                 .Skip(card.CardId)
                 .Take(card.Matches);
-            
+
             // Iterate through the won copies and add them to the wonCards dictionary
             foreach (var copy in copies)
             {
-                if (wonCards.ContainsKey(copy.CardId))
-                {
-                    wonCards[copy.CardId]++;
-                }
-                else
-                {
-                    wonCards.Add(copy.CardId, 1);
-                }
+                sum++;
 
                 // Recursive call for copies with matches
                 if (copy.Matches > 0)
@@ -76,12 +60,14 @@ public sealed partial class Day04 : BaseDay
     private List<Card> GetCards()
     {
         var cards = new List<Card>();
-        
+
         for (var i = 0; i < _input.Length; i++)
         {
             var game = _input[i];
+
             var splitOne = game.Split(":");
             var numbersSplit = splitOne[1].Split("|");
+
             var winningNumbers = Regex.Split(numbersSplit[0].Trim(), @"\D+");
             var numbersIHave = Regex.Split(numbersSplit[1].Trim(), @"\D+");
 
@@ -90,15 +76,14 @@ public sealed partial class Day04 : BaseDay
                 .Select((number, index) => new { Number = number, Index = index })
                 .Aggregate(0, (total, next) => next.Index == 0 ? 1 : total * 2);
 
-            
-            var matches = (from numberIHave in numbersIHave
-                where winningNumbers.Contains(numberIHave)
-                select numberIHave)
+            var matches = numbersIHave
+                .Where(numberIHave => winningNumbers.Contains(numberIHave))
+                .Select(numberIHave => numberIHave)
                 .Count();
 
             cards.Add(new Card(i + 1, matches, points));
         }
-        
+
         return cards;
     }
 }
