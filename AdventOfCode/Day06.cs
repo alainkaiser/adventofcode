622102272541a18e9b029b1cad6063b2a214bad8
Alain Kaiser <kaiseralain1@gmail.com>
2023-12-06 11:00:35 +0100

    wip: init day06

---
+++ AdventOfCode/Day06.cs
@@ -0,0 +1,21 @@
+namespace AdventOfCode;
+
+public sealed partial class Day06 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day06()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
