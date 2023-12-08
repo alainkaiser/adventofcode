cc747e1ad569d407a70895449fbf8d121e64c9b2
Alain Kaiser <kaiseralain1@gmail.com>
2023-12-08 08:28:04 +0100

    wip: add day 6

--- AdventOfCode/Day06.cs
+++ AdventOfCode/Day06.cs
@@ -1,8 +1,15 @@
+using System.Text.RegularExpressions;
+
 namespace AdventOfCode;
 
 public sealed partial class Day06 : BaseDay
 {
     private readonly string[] _input;
+
+    [GeneratedRegex(@"\d+")]
+    private static partial Regex DigitRegex();
+
+    private record Race(long Duration, long RecordDistance);
 
     public Day06()
     {
@@ -11,11 +18,68 @@ public sealed partial class Day06 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        throw new NotImplementedException();
+        var races = ParseRaces(_input);
+        var winningWaysPerRace = races.Values
+            .Select(GetWinningWays)
+            .ToList();
+
+        var result = winningWaysPerRace.Aggregate((total, next) => total * next);
+
+        return new ValueTask<string>(result.ToString());
     }
 
     public override ValueTask<string> Solve_2()
     {
-        throw new NotImplementedException();
+        var races = ParseRaces(_input);
+
+        var realRaceTime = string.Empty;
+        var realRaceDistance = string.Empty;
+
+        foreach (var race in races.Values)
+        {
+            realRaceTime += race.Duration;
+            realRaceDistance += race.RecordDistance;
+        }
+
+        var winningWays = GetWinningWays(new Race(long.Parse(realRaceTime), long.Parse(realRaceDistance)));
+
+        return new ValueTask<string>(winningWays.ToString());
+    }
+
+    private int GetWinningWays(Race race)
+    {
+        var winningWays = 0;
+
+        for (var startTime = 0; startTime <= race.Duration; startTime++)
+        {
+            var playTime = race.Duration - startTime;
+            var doneDistance = startTime * playTime;
+
+            if (doneDistance > race.RecordDistance)
+            {
+                winningWays++;
+            }
+        }
+
+        return winningWays;
+    }
+
+    private Dictionary<int, Race> ParseRaces(string[] input)
+    {
+        var racesDic = new Dictionary<int, Race>();
+
+        var times = DigitRegex().Matches(input[0])
+            .Select(m => int.Parse(m.Value))
+            .ToArray();
+        var distances = DigitRegex().Matches(input[1])
+            .Select(m => int.Parse(m.Value))
+            .ToArray();
+
+        for (var i = 0; i < times.Length; i++)
+        {
+            racesDic.Add(i, new Race(times[i], distances[i]));
+        }
+
+        return racesDic;
     }
 }
