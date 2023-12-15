2cc9db21bcd8eb4066ab3bd85771758234ef94a7
Alain Kaiser <kaiseralain1@gmail.com>
2023-12-15 08:58:27 +0100

    wip: added empty day13

---
+++ AdventOfCode/Day13.cs
@@ -0,0 +1,21 @@
+namespace AdventOfCode;
+
+public sealed class Day13 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day13()
+    {
+        _input = File.ReadAllLines(InputFilePath);
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        throw new NotImplementedException();
+    }
+}
